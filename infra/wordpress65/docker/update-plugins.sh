0e8f4177377b542d88ecc860dd09a77b06b95b1f
Sang Nguyen Thanh <sang.nguyenthanh@nfq.com>
2024-07-08 11:27:24 +0000

    Update update-plugins.sh

--- infra/wordpress65/docker/update-plugins.sh
+++ infra/wordpress65/docker/update-plugins.sh
@@ -17,14 +17,6 @@ if [ -z "$GITLAB_USERNAME" ] || [ -z "$GITLAB_TOKEN" ]; then
   exit 1
 fi
 
-# Install woocommerce plugin
-curl -k -L --output $WOO_VERSION.zip https://downloads.wordpress.org/plugin/$WOO_VERSION.zip
-unzip $WOO_VERSION.zip
-rm $WOO_VERSION.zip
-mkdir -p $PLUGIN_FOLDER/woocommerce
-cp -rf woocommerce/. $PLUGIN_FOLDER/woocommerce
-rm -r woocommerce
-
 # Install Twint woocommerce plugin
 mkdir -p $PLUGIN_FOLDER/woocommerce-gateway-twint
 git clone -b $TWINT_BRANCH --single-branch https://$GITLAB_USERNAME:$GITLAB_TOKEN@git.nfq.asia/twint-ag/woo-extension.git woocommerce-gateway-twint
