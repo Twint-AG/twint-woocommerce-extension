0530ed456be9ed86ac53193ba41a385ff4a68f19
Jimmy Nguyen <jimmy.nguyenminh@nfq.com>
2024-09-05 13:44:06 +0700

    updated npm build js

--- assets/js/frontend/blocks.asset.php
+++ assets/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '842e73f7efc58e453383');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'e4360349ea38ef82bbf0');
