265da15620312c3795e7d2efb239b3d75155b450
Jimmy Nguyen <jimmy.nguyenminh@nfq.com>
2024-10-08 14:29:16 +0700

    updated wp-config

--- infra/oldest/wp-config.php
+++ infra/oldest/wp-config.php
@@ -109,6 +109,10 @@ $table_prefix = getenv_docker('WORDPRESS_TABLE_PREFIX', 'wp_');
 define('WP_DEBUG', !!getenv_docker('WORDPRESS_DEBUG', ''));
 define('WP_AUTO_UPDATE_CORE', false);
 
+define( 'WP_DEVELOPMENT_MODE', 'plugin' );
+define('WP_DEBUG_DISPLAY', false);
+define('WP_DEBUG_LOG', true);
+
 /* Add any custom values between this line and the "stop editing" line. */
 
 // If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
