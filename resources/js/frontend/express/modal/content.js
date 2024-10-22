313199e3507d2bfd3170b181b193ef5a729f932d
Tai Tran <ngoctai.tran@nfq.com>
2024-10-22 12:27:37 +0700

    fix JS code styles

--- resources/js/frontend/express/modal/content.js
+++ resources/js/frontend/express/modal/content.js
@@ -48,10 +48,9 @@ class ModalContent {
     if (this.isExpress) {
       selectContact.classList.remove('hidden')
 
-      if(!selectContact.closest('.twint-mobile'))
+      if (!selectContact.closest('.twint-mobile'))
         guides.classList.add('md:grid-cols-2')
-      else
-        guides.classList.remove('md:grid-cols-2')
+      else guides.classList.remove('md:grid-cols-2')
     } else {
       selectContact.classList.add('hidden')
       guides.classList.remove('md:grid-cols-2')
