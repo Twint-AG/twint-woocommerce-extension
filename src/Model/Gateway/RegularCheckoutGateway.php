decb07a98a0ab994a2926fb416c1c700a6da1391
Phat Nham <phattannham@gmail.com>
2026-05-05 15:57:00 +0700

    fix: return empty string instead of false for missing URL in checkout gateway

--- src/Model/Gateway/RegularCheckoutGateway.php
+++ src/Model/Gateway/RegularCheckoutGateway.php
@@ -190,7 +190,7 @@ class RegularCheckoutGateway extends AbstractGateway
                     $url
                 );
             } else {
-                $url = false;
+                $url = '';
             }
 
             return [
